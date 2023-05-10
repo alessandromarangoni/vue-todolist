b7079318fd67384cd1a2a12f2731a3740859dc47
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-10 17:25:05 +0200

    manca classe per line-through continuo stase

---
+++ script.js
@@ -0,0 +1,33 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            nuova:{
+                daFare:"",
+                fatto:false
+            },
+            cose:[
+                {daFare:"prendi pasta",fatto:false},
+                {daFare:"metti acqua",fatto:false},
+                {daFare:"accendi foco",fatto:true},
+                {daFare:"40 minutes",fatto:false},
+                {daFare:"mhua",fatto:false}
+            ],
+            fatta:false
+        }
+    },
+    methods:{
+        aggiungi(){
+            let add = {...this.nuova};
+            this.cose.push(add)
+        },
+        rimuovi(pos){
+            this.cose.splice(pos,1)
+        },
+        fatto(pos){
+            this.cose[pos].fatto = !this.cose[pos].fatto
+            console.log(this.cose.fatto)
+        }
+    }
+}).mount('#app')
